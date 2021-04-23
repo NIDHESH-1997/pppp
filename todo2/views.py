f2f1d2c5c5f032b9ad80eb3902ee476b1a253428
NIDHESH-1997 <76711630+NIDHESH-1997@users.noreply.github.com>
2021-04-23 09:47:18 +0530

    Update views.py

--- todo2/views.py
+++ todo2/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import TODOFORM
 from .models import TODO
-from dkango.views.generic import TemplateView
+from django.views.generic import TemplateView
 
 # Create your views here.
 
